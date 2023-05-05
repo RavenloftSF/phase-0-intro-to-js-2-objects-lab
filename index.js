c3387d6bea7c414497d3cb0ed281a5107121ef19
JosephBorrelli <josephborrelli80@gmail.com>
2023-05-05 15:40:39 -0400

    did the thing

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: "apple",
+    streetAddress: "depecheMode",
+  };
+  
+function updateEmployeeWithKeyAndValue(employeeObj, key, value){
+    let empObjCopy = {...employeeObj}
+    empObjCopy[key] = value 
+    return empObjCopy
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value 
+    return employee
+}
+
+function deleteFromEmployeeByKey(employeeObj, key){
+    let empObjCopy = {...employeeObj}
+    delete empObjCopy[key] 
+    return empObjCopy
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
